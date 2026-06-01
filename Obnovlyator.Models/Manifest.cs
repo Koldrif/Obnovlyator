5b5192accac7c5918d3c8f20dec3495f1b992043
Igor <vorobiov.777@yandex.ru>
2026-06-01 21:16:55 +0300

    Added manifest generation

---
+++ Obnovlyator.Models/Manifest.cs
@@ -0,0 +1,23 @@
+﻿namespace Obnovlyator.Models;
+
+public record Manifest
+{
+	public Manifest() : this(DateTime.Now)
+	{
+		
+	}
+
+	public Manifest(DateTime version)
+	{
+		Version = version.ToString("yyyy-MM-dd-hh-mm-ss");
+		Files = new();
+	}
+	public string Version { get; set; }
+	public List<ManifestFileInfo> Files { get; set; }
+}
+
+public record ManifestFileInfo
+{
+	public string Path;
+	public string SHA; 
+}
--- Obnovlyator.Server/Program.cs
+++ Obnovlyator.Server/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+using Newtonsoft.Json;
+using Obnovlyator.Models;
+
+var exeDirectory = Environment.CurrentDirectory;
+var manifest = new Manifest();
+
+var tasks = new List<Task<ManifestFileInfo>>();
+foreach (var filePath in Directory.EnumerateFiles(exeDirectory, "*", SearchOption.AllDirectories))
+{
+	tasks.Add(Task.Run(() =>
+	{
+		var fileInfo = File.OpenRead(filePath);
+		var sha = Convert.ToHexString(SHA256.HashData(fileInfo));
+		return new ManifestFileInfo()
+		{
+			Path = Path.GetRelativePath(exeDirectory, filePath),
+			SHA = sha
+		};
+	}));
+}
+
+var fileInfos = await Task.WhenAll(tasks);
+
+manifest.Files = fileInfos.ToList();
+
+// Console.WriteLine(JsonSerializer.Serialize(manifest, new JsonSerializerOptions() {WriteIndented = true, IncludeFields = true}));
+Console.WriteLine(JsonConvert.SerializeObject(manifest, Formatting.Indented));
