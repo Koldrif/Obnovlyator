2f167348ab341805cfbfb33300043f1935a41e16
Igor <vorobiov.777@yandex.ru>
2026-06-02 13:40:47 +0300

    Moving from single file build to normal

--- Obnovlyator.Server/ManifestCreator.cs
+++ Obnovlyator.Server/ManifestCreator.cs
@@ -6,6 +6,7 @@ namespace Obnovlyator.Server;
 public static class ManifestCreator
 {
 	public static readonly string[] filesToExclude = [Settings.ManifestFileName, "Obnovlyator.Server.exe"];
+	public static readonly string[] directoriesToExclude = [Settings.ExecLocation];
 	public static async Task<Manifest> Create(string? exeDirectory = null)
 	{
 		var manifest = new Manifest();
@@ -19,6 +20,11 @@ public static class ManifestCreator
 		foreach (var filePath in Directory.EnumerateFiles(exeDirectory, "*", SearchOption.AllDirectories))
 		{
 			if (filesToExclude.Contains(Path.GetFileName(filePath)))
+			{
+				continue;
+			}
+
+			if (directoriesToExclude.Any(s => filePath.Contains(s)))
 			{
 				continue;
 			}
--- Obnovlyator.Server/Program.cs
+++ Obnovlyator.Server/Program.cs
@@ -2,7 +2,7 @@
 using Obnovlyator.Models;
 using Obnovlyator.Server;
 
-var manifest = await ManifestCreator.Create(Settings.ExecLocation);
+var manifest = await ManifestCreator.Create(Settings.OperatingDirectory);
 
 var manifestJson = JsonConvert.SerializeObject(manifest, Formatting.Indented);
-await File.WriteAllTextAsync(Path.Combine(Settings.ExecLocation, Settings.ManifestFileName), manifestJson);
+await File.WriteAllTextAsync(Path.Combine(Settings.OperatingDirectory, Settings.ManifestFileName), manifestJson);
